65316808124efb337a83ccf359335eab1730fcae
Geoff Russell <Geoff.R.Russell@gmail.com>
2015-02-12 10:23:29 -0800

    Added animation to newly added suppliers

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 $(document).ready(function(){
      $('button').click(function () {
      	var supplierName = $('input[name=supplierName]').val();
-     	$('.supplierList').append('<div class="item">Supplier: '+ supplierName + '</div>');
+     	var industryName = $('input[name=industry]').val();
+     	/*jshint multistr: true */
+     	var itemData = "<div class='item' style='display:none'> \
+     		Supplier: " + supplierName + "\
+     		Industry: " + industryName + "</div>";
+     	$(itemData).appendTo('.supplierList').fadeIn('slow');
      });
      $('input').focus(function () {
           $(this).css('outline-color', '#FF0000');
