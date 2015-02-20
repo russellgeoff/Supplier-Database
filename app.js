c1759eab8e1524d0a4c16a9f2052fb6f63090c4b
Geoff Russell <Geoff.R.Russell@gmail.com>
2015-02-19 16:46:44 -0800

    Uses angular to read from JSON and save to JSON

--- app.js
+++ app.js
@@ -1,6 +1,53 @@
 angular.module('myApp', [])
-.controller('myController', function($scope) {
+.controller('myController', function($scope, $http) {
   $scope.user = {
-    name: 'Alex'
+    name: 'Supplier'
+  };
+
+  $scope.newSupplier = {
+    name:"Default Name",
+    location:"Default Location",
+    industry:"Default Industry"
+  };
+
+  $scope.suppliers =[
+  {
+    name:"test",
+    location:"places"
+  },
+  {
+    name:"test2",
+    location:"places3"
+  }];
+
+  $http.get('suppliers.json')
+  .success(function (response) {
+    console.log($scope.suppliers);
+    $scope.suppliers = response;
+  })
+  .error(function(err) {
+    alert(err);
+  });
+
+  $scope.add = function () {
+    var name = $scope.newSupplier.name;
+    var location = $scope.newSupplier.location;
+    var industry = $scope.newSupplier.industry;
+    console.log($scope.suppliers);
+    $scope.suppliers.push({
+      name:name,
+      location:location,
+      industry:industry
+    });
+  };
+
+  $scope.save = function () {
+    $http.post('suppliers.json', $scope.suppliers)
+    .success(function (response) {
+      alert('Save successful');
+    })
+    .error(function(err){
+      alert(err);
+    });
   };
 });
--- script.js
+++ script.js
@@ -1,14 +1,4 @@
 $(document).ready(function(){
-     $('button').click(function () {
-     	var supplierName = $('input[name=supplierName]').val();
-     	var industryName = $('input[name=industry]').val();
-     	/*jshint multistr: true */
-     	var itemData = "<div class='item' style='display:none'> \
-     		Supplier: " + supplierName + "\
-     		Industry: " + industryName + "</div>";
-     	$(itemData).appendTo('.supplierList').fadeIn('slow');
-     });
-
      $('input').focus(function () {
           $(this).css('outline-color', '#FF0000');
      });
