78ad26b3188f86acc191a47fbe8a9dc341e10a79
Geoff Russell <Geoff.R.Russell@gmail.com>
2015-03-18 22:50:03 -0700

    Refactored code to take advantage of bootstrap stylings

--- app.js
+++ app.js
@@ -2,10 +2,6 @@ angular.module('myApp', ['firebase'])
 .controller('myController', function($scope, $http, $firebaseArray) {
   var ref = new Firebase('https://brilliant-heat-5688.firebaseio.com/');
 
-  $scope.user = {
-    name: 'Supplier'
-  };
-
   $scope.newSupplier = {
     name:"Default Name",
     location:"Default Location",
@@ -14,24 +10,17 @@ angular.module('myApp', ['firebase'])
 
   $scope.suppliers =[
   {
-    name:"test",
-    location:"places"
+    name:'Default Supplier Name 1',
+    location:'Default Supplier Location 1',
+    industry:'Default Industry 1'
   },
   {
-    name:"test2",
-    location:"places3"
+    name:'Default Supplier Name 2',
+    location:'Default Supplier Location 2',
+    industry:'Default Industry 2'
   }];
 
   $scope.suppliers = $firebaseArray(ref);
-
-  // $http.get('suppliers.json')
-  // .success(function (response) {
-  //   console.log($scope.suppliers);
-  //   $scope.suppliers = response;
-  // })
-  // .error(function(err) {
-  //   alert(err);
-  // });
 
   $scope.add = function () {
     var name = $scope.newSupplier.name;
@@ -42,16 +31,6 @@ angular.module('myApp', ['firebase'])
       name:name,
       location:location,
       industry:industry
-    });
-  };
-
-  $scope.save = function () {
-    $http.post('suppliers.json', $scope.suppliers)
-    .success(function (response) {
-      alert('Save successful');
-    })
-    .error(function(err){
-      alert(err);
     });
   };
 });
